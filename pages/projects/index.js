385d28a349ff24c47578ef093f4283ff213add4a
oscarCorb <info@oscarcorb.com>
2022-02-17 22:00:09 +0100

    add: load more button

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,7 +1,16 @@
+import styles from './index.module.css';
+
 const Projects = () => {
   return (
-    <div>
-      <h2>Projects</h2>
+    <div className={styles.main_container}>
+      <div className={styles.gallery}>
+        <div>Galería de imágenes</div>
+      </div>
+      <div className={styles.load_more_container}>
+        <div className={styles.vertical_line}></div>
+        <button className={styles.load_more_button}>Load more projects</button>
+        <div className={styles.vertical_line}></div>
+      </div>
     </div>
   );
 };

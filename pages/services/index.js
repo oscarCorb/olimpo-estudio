a4d203fcaa1f95fb560ddc4f41306b7345472cfd
oscarCorb <info@oscarcorb.com>
2022-02-17 22:53:14 +0100

    add: services section

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,8 +1,18 @@
+import styles from './index.module.css';
+
 const Services = () => {
   return (
-    <div>
-      <h2>Services</h2>
-    </div>
+    <>
+      <div className={styles.main_container}>
+        <h2 className={styles.section_name}>Services</h2>
+        <h3 className={styles.section_title}>Reformas y rehabilitación</h3>
+        <div className={styles.description}>
+          <p>Servicio integral de reformas y rehabilitación</p>
+          <p>Servicio de seguimiento de obra</p>
+          <p>Servicio de evaluación y propuesta básica de las necesidades del proyecto</p>
+        </div>
+      </div>
+    </>
   );
 };
 
